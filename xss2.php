613df3ca9830036d169e74df1ebe89b3cd2242fd
shieldhub <danesh@shieldhub.in>
2018-09-24 15:58:04 +0530

    xss persistence

--- xss2.php
+++ xss2.php
@@ -6,29 +6,24 @@ Message: <input type="text" name="message"><br>
 <input type="submit">
 </form>
 
-<?php 
+<?php
 	$conn = mysqli_connect('localhost', 'root', '', 'testbed');
 	$message = $_POST["message"];
-	
-	$sql = "INSERT INTO xss2 (message) VALUES ('$message')";
-	$conn->query($sql);	
-?>
+	//$message = htmlentities($message);
 
-<br>msg: <?php echo $_POST["message"]; ?><br>
+	$sql = "INSERT INTO xss2 (message) VALUES ('$message')";
+	$conn->query($sql);
+
+	$sql = "select message from xss2";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+    		while($row = $result->fetch_assoc()) {
+        		echo "Message: " . $row["message"]. "<br>";
+   		}
+	}else 	{
+    		echo "0 results";
+		}
+?>
 
 </body>
-</html> 
-
-
-
-<?php
-
-//$conn = new mysqli('localhost', 'root', '', 'testbed');
-
-//$sql = "INSERT INTO xss2 (message)
-//VALUES ('xxxxxxxxxx')";
-
-//$conn->query($sql);
-
-//$conn->close();
-?>
+</html>
